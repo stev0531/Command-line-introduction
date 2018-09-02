95abcdc57d745dfea4fb3560eab3ccb4d7d4b91c
stev0531 <stev0531@morris.umn.edu>
2018-09-02 14:34:48 -0500

    Added to the `extract_and_compile.sh` so it compiles

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,6 +1,19 @@
-#!/usr/bin/bash
+#!/bin/bash
+#Extracts and uncompresses the NthPrime.tgz file 
+#and places the files in the specified directory.
+#Compiles and runs the files in the tar file with
+#the specified number to give the Prime of that number.
+
+#Elizabeth Stevens and Mark Schauer
+#University of Minnesota Morris
+#September 2018
+
 
 num=$1
 tar_file=$2
 
 tar zxf NthPrime.tgz --directory $tar_file
+cd $tar_file/NthPrime/
+gcc main.c nth_prime.h nth_prime.c -o NthPrime
+
+./NthPrime $num
