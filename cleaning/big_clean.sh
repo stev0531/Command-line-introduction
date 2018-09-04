537cf85eef2b9073ecac3444a5cb0cbcdab36f2c
Saturable <marksthe1@gmail.com>
2018-09-04 11:02:50 -0500

    Reworked code to correctly remove and compress files Now not specifically `little_dir` so it works with other tar files.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+# Elizabeth Stevens and Mark Schauer
+# U of M Morris
+# Fall 2018
+
+# Variable names
 name=$1
 scratch=$2
-
-tar zxf $name --directory $scratch
 here=$(pwd)
+
+# Extracts the given tar file into the scratch directory
+tar -zxf $name --directory $scratch
 cd $scratch
-cd little_dir
+# Variable created to only fetch the first portion of the name given above
+noext=`basename $name ".tgz"`
+# Enter that directory with the variable created above
+cd $noext
+# Use a remove function to delete files with DELETE in them
 rm `grep -l DELETE *`
-cd $here
-tar zcf cleaned_$name $scratch
+cd ..
+# Recompress the directory
+tar -zcf $here/cleaned_$name $noext
